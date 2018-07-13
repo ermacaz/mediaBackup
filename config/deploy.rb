87162dae7acc9a150183c12423f64b89d6392744
Matt Hamada <mhamada@softeligent.com>
2018-07-13 08:06:27 -0700

    convert more to api

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/master.key config/database.yml config/application_config.rb}
-set :linked_dirs,  %w{ bin log tmp/pids tmp/images tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{log tmp/pids tmp/images tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
